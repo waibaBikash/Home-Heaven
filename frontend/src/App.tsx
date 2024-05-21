6033cce3630b4606ce4d0bd3bb12d3f8c7ea37c5
waibaBikash <kavrelibikash@gmail.com>
2024-05-22 01:01:08 +1000

    create a sign in form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router , Route, Routes, Navigate,} from "react-router-dom";
 import Layout from "./layouts/Layout";
+import Register from "./pages/Register";
 
 const App = () => {
   return(
@@ -11,6 +12,11 @@ const App = () => {
         <Route path="/search" element={<Layout>
           <p>Search Page</p>
         </Layout>} />
+        <Route path="/register" element={
+          <Layout>
+            <Register/>
+          </Layout>
+        } />
         <Route path="/*" element={<Navigate to="/" />} />
       </Routes>
    </Router>
