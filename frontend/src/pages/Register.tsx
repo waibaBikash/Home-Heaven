e919fc6060939b1ec4b6b13c24be531c3398cf72
waibaBikash <kavrelibikash@gmail.com>
2024-05-23 20:32:56 +1000

    save change

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,4 +1,6 @@
 import { useForm } from "react-hook-form";
+import { useMutation } from "react-query";
+import * as apiClient from '../api-client';
 
 
  export type RegisterFormData = {
@@ -15,8 +17,17 @@ const Register = () => {
     formState: { errors },
    } = useForm<RegisterFormData>();
 
+   const mutation = useMutation(apiClient.register, {
+     onSuccess: () => {
+      console.log("registration successful!");
+     },
+     onError: (error: Error)=>{
+       console.log(error.message);
+     },
+   });
+
    const onSubmit = handleSubmit((data)=>{
-     console.log(data);
+     mutation.mutate(data);
    })
   return (
     <form className="flex flex-col gap-5" onSubmit={onSubmit}>
